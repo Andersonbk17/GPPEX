0abc578a564af85c66e644d864cae389335d797c
Anderson <Anderson@darkside>
2015-05-25 14:05:11 -0300

    criacao da pesquisa

---
+++ application/models/Pesquisa.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Pesquisa extends CI_Model{
+    
+    private $titulo;
+    private $numeroCadastro;
+    private $areaConhecimento;
+    private $grupoPesquisa;
+    private $idCampus;
+    
+    
+}
